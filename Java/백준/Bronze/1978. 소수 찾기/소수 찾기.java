500dcc752696f4900a4920ce1a7de295e3c4a869
juwon0124 <2025245@sungil-i.kr>
2026-04-13 17:14:36 +0900

    Update 소수 찾기.java

--- Java/백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ Java/백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -5,23 +5,23 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         
         int N = sc.nextInt(); // 몇 개 입력받을 건지
-        int count = 0; // 소수 개수 세는 변수
+        int count = 0; // 소수의 개수 세는 변수임
 
         for (int i = 0; i < N; i++) {
             int num = sc.nextInt();
-            if (num < 2) continue; // 1은 어차피 소수 아니니까 패스한다
+            if (num < 2) continue; // 1은 어차피 소수 아니니까 패스해야 한다.
             
             boolean check = true; // 일단 소수라고 치고 시작
             for (int j = 2; j * j <= num; j++) {
                 if (num % j == 0) {
-                    check = false; // 나눠지면 소수 탈락
+                    check = false; // 나눠지면 소수 탈락이다.
                     break;
                 }
             }
-            if (check) count++; // 탈락 안 했으면 카운트+1
+            if (check) count++; // 탈락 안 했으면 카운트+1 증가한다.
         }
 
-        System.out.println(count); // 결과 출력
+        System.out.println(count); // 결과 출력한다
         sc.close();
     }
 }
